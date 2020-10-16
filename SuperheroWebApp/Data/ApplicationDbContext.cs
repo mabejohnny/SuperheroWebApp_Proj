6dd7a71a9d7b20a42e33db29146cb646a35ba58c
Josh Grimaldi <69489227+mabejohnny@users.noreply.github.com>
2020-10-16 09:57:49 -0400

    Initial commit with setup of superhero class member variables, superhero controller, Table built.

---
+++ SuperheroWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SuperheroWebApp.Models;
+
+namespace SuperheroWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Superhero> Superheroes { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ SuperheroWebApp/Models/Superhero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperheroWebApp.Models
+{
+    public class Superhero
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display(Name = "Alter Ego")]
+        public string AlterEgo { get; set; }
+        [Display(Name = "Primary Ability")]
+        public string PrimaryAbility { get; set; }
+        [Display(Name = "Secondary Ability")]
+        public string SecondaryAbility { get; set; }
+        [Display(Name = "Catch Phrase")]
+        public string CatchPhrase { get; set; }
+    }
+}
